56f9cbacd553e07e77b8456df2cc701856a18784
Hien Tran <hien.tran@dkb.de>
2021-06-17 15:13:50 +0200

    Initial Commit

---
+++ src/test/java/com/ironhack/MinIProject_ConfigServer/MinIProjectConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.MinIProject_ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinIProjectConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
